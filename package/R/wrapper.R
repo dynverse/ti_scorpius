ce1778baed2f5c585f836bfa4c4c467c8a6cbb09
Wouter Saelens <wouter.saelens@gmail.com>
2019-04-10 16:46:01 +0200

    update scorpius docs

--- package/R/wrapper.R
+++ package/R/wrapper.R
@@ -70,10 +70,9 @@ run_fun <- function(expression, priors, parameters, seed = NULL, verbose = 0)  {
 
 definition <- dynwrap:::.method_load_definition(system.file("definition.yml", package = "tiscorpius"))
 
-#' @import dplyr
-#' @import purrr
-#' @import SCORPIUS
+#' Infer a trajectory using SCORPIUS
 #'
+#' @eval dynwrap::generate_parameter_documentation(definition)
 #' @export
 ti_scorpius <- dynwrap::create_ti_method_r(
   definition = definition,
